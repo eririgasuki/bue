262fd9345fed20575001777f9a4a25fee567d8e8
eririgasuki <791121579@qq.com>
2019-03-24 14:40:12 +0800

    加入parcel，将button模块变成Vue的单个组件文件

--- button.js
+++
@@ -1,5 +0,0 @@
-Vue.component('b-button',{
-    template:`
-        <button class="b-button">提交</button>
-    `
-})
---
+++ src/button.vue
@@ -0,0 +1,28 @@
+<template>
+  <button class="b-button">提交</button>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+.b-button {
+  height: var(--button-height);
+  font-size: var(--font-size);
+  padding: 0 1em;
+  border-radius: var(--border-radius);
+  border: 1px solid var(--border-color);
+  background: var(--button-bg);
+  &:hover {
+    border-color: var(--border-color-hover);
+  }
+  &:active {
+    background-color: var(--button-active-bg);
+  }
+  &:focus {
+    outline: none;
+  }
+}
+</style>
+
