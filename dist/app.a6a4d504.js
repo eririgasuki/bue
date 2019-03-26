129009778b3b8097f13599a985cad998734c33c1
eririgasuki <791121579@qq.com>
2019-03-26 16:05:49 +0800

    gitignore dist folder

--- dist/app.a6a4d504.js
+++ dist/app.a6a4d504.js
@@ -12506,7 +12506,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "4882" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "3408" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
