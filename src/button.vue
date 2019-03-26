df0676355f02844a7644ea7f7189b7dd1d022642
eririgasuki <791121579@qq.com>
2019-03-26 23:23:01 +0800

    添加icon和iconPosition属性

--- src/button.vue
+++ src/button.vue
@@ -1,19 +1,33 @@
 <template>
-  <button class="b-button">提交</button>
+  <button class="b-button" :class="{[`icon-${iconPosition}`]:true}">
+    <svg v-if="icon" class="icon">
+      <use :xlink:href="`#i-${icon}`"></use>
+    </svg>
+    <div class="content">
+      <slot></slot>
+    </div>
+  </button>
 </template>
 
 <script>
-export default {};
+export default {
+  props: ["icon", "iconPosition"]
+};
 </script>
 
 <style lang="scss" scoped>
 .b-button {
   height: var(--button-height);
   font-size: var(--font-size);
+  line-height: var(--font-size);
   padding: 0 1em;
   border-radius: var(--border-radius);
   border: 1px solid var(--border-color);
   background: var(--button-bg);
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  vertical-align: top;
   &:hover {
     border-color: var(--border-color-hover);
   }
@@ -22,6 +36,23 @@ export default {};
   }
   &:focus {
     outline: none;
+  }
+  > .content {
+    order: 2;
+  }
+  > .icon {
+    order: 1;
+    margin-right: .1em;
+  }
+  &.icon-right {
+    > .content {
+      order: 1;
+    }
+    > .icon {
+      order: 2;
+      margin-right: 0;
+      margin-left: .1em;
+    }
   }
 }
 </style>
