59391c5f9940e42df1b545a8fcc091eed3f6cdc1
eririgasuki <791121579@qq.com>
2019-04-03 17:11:04 +0800

    重构row和col

--- src/col.vue
+++ src/col.vue
@@ -1,15 +1,38 @@
 <template>
-  <div class="col" :class="[`col-${span}`]">
+  <div class="col" :class="ColClass" :style="ColStyle">
     <slot></slot>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'BueCol',
-  props:{
-    span:{
+  name: "BueCol",
+  props: {
+    span: {
       type: [Number, String]
+    },
+    offset: {
+      type: [Number, String]
+    }
+  },
+  data() {
+    return {
+      gutter: {
+        type: [Number, String]
+      }
+    };
+  },
+  computed: {
+    ColClass() {
+      let { span, offset } = this;
+      return [span && `col-${span}`, offset && `offset-${offset}`];
+    },
+    ColStyle() {
+      let { gutter } = this;
+      return {
+        paddingLeft: gutter / 2 + "px",
+        paddingRight: gutter / 2 + "px"
+      };
     }
   }
 };
@@ -19,12 +42,16 @@ export default {
 .col {
   width: 50%;
   height: 100px;
-  background-color: #666666;
-  border: 1px solid #000;
   $class-prefix: col-;
   @for $n from 1 through 24 {
     &.#{$class-prefix}#{$n} {
       width: ($n/24) * 100%;
+    }
+  }
+  $class-prefix: offset-;
+  @for $n from 1 through 24 {
+    &.#{$class-prefix}#{$n} {
+      margin-left: ($n/24) * 100%;
     }
   }
 }
--- src/row.vue
+++ src/row.vue
@@ -1,11 +1,32 @@
 <template>
-  <div class="row">
+  <div class="row" :style=RowStyle>
     <slot></slot>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  name: "BueRow",
+  props: {
+    gutter: {
+      type: [Number, String]
+    }
+  },
+  computed: {
+    RowStyle() {
+      let {gutter} = this
+      return {
+        marginRight: -gutter / 2 + "px",
+        marginLeft: -gutter / 2 + "px"
+      };
+    }
+  },
+  mounted() {
+    this.$children.forEach(vm => {
+      vm.gutter = this.gutter;
+    });
+  }
+};
 </script>
 
 <style lang="scss" scoped>
