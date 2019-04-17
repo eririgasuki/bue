67e03f4164836a483b50c9b7ac6b71fbb3055c5e
eririgasuki <791121579@qq.com>
2019-04-17 14:30:24 +0800

    实现popover功能

--- src/app.js
+++ src/app.js
@@ -17,6 +17,7 @@ import TabsHead from './tabs-head'
 import TabsBody from './tabs-body'
 import TabsItem from './tabs-item'
 import TabsPane from './tabs-pane'
+import Popover from './popover'
 
 
 Vue.component('b-button', Button)
@@ -37,6 +38,7 @@ Vue.component('b-tabs-head', TabsHead)
 Vue.component('b-tabs-body', TabsBody)
 Vue.component('b-tabs-item', TabsItem)
 Vue.component('b-tabs-pane', TabsPane)
+Vue.component('b-popover', Popover)
 
 
 new Vue({
---
+++ src/popover.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="popover" @click.stop="xxx">
+    <div ref="contentWrapper" class="content-wrapper" v-if="visible" @click.stop>
+      <slot name="content"></slot>
+    </div>
+    <span ref="triggerWrapper">
+      <slot></slot>
+    </span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "BuePopover",
+  props: {},
+  data() {
+    return {
+      visible: false
+    };
+  },
+  watch: {},
+  computed: {},
+  methods: {
+    xxx() {
+      this.visible = !this.visible;
+      if (this.visible === true) {
+        this.$nextTick(() => {
+          document.body.appendChild(this.$refs.contentWrapper);
+          let {
+            width,
+            height,
+            top,
+            left
+          } = this.$refs.triggerWrapper.getBoundingClientRect();
+          this.$refs.contentWrapper.style.top = top + window.scrollX + "px";
+          this.$refs.contentWrapper.style.left = left + window.scrollY + "px";
+          let eventHandler = () => {
+            this.visible = false;
+            document.removeEventListener("click", eventHandler);
+          };
+          document.addEventListener("click", eventHandler);
+        });
+      }
+    }
+  },
+  created() {},
+  mounted() {}
+};
+</script>
+<style lang="scss" scoped>
+.popover {
+  display: inline-block;
+  vertical-align: top;
+  position: relative;
+}
+.content-wrapper {
+  position: absolute;
+  border: 1px solid red;
+  box-shadow: 0 0 3px rgba(0, 0, 0, 0.5);
+  transform: translateY(-100%);
+}
+</style>
